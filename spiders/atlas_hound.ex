d63dc54c0bd5d00603aa272dd76e875ed188f1f6
faezlatiff <muhammadal.2021@scis.smu.edu.sg>
2023-07-31 11:52:06 +0800

    Fixed date issues, added end cases

--- spiders/atlas_hound.ex
+++ spiders/atlas_hound.ex
@@ -1,6 +1,9 @@
 defmodule Atlas.Hound do
   use Hound.Helpers
-  @url "https://www.burpple.com/neighbourhoods/sg/chinatown"
+  @base_url "https://www.burpple.com"
+  @neighbourhood "chinatown"
+  @url "https://www.burpple.com/neighbourhoods/sg/#{@neighbourhood}"
+  @limit 4000
 
   @spec run :: list
   def run do
@@ -16,8 +19,7 @@ defmodule Atlas.Hound do
 
   end
 
-  defp scrape(list, file_path, offset) do
-    IO.inspect(offset)
+  defp scrape(list, file_path, offset) when offset < @limit do
     Enum.drop(list, offset) |>
      Enum.each(fn element ->
         post_map =
@@ -25,19 +27,23 @@ defmodule Atlas.Hound do
           |> Floki.parse_document()
           |> into_map()
           # |> check_date()
-          |> maybe_store_and_click(file_path, offset)
+          |> store(file_path, offset)
     end)
     check_visible_and_click(file_path, offset)
 
   end
+
+  defp scrape(:scrape_limit_reached, _, _), do: IO.puts("Scraped 4000 items, scraper stopping... \nTo scrape more items, change the @limit setting.")
+  defp scrape(:no_more_data), do: IO.puts("No more data to scrape, scraper stopping...")
 
   defp into_map({:ok, html_tree}) do
     name = Floki.find(html_tree, ".food-venue-detail--title") |> get_inner_text()
     address = Floki.find(html_tree, ".food-venue-detail--address") |> get_inner_text()
     title = Floki.find(html_tree, ".food-description-title") |> get_inner_text()
     body = Floki.find(html_tree, ".food-description-body") |> get_inner_text()
-    username = Floki.find(html_tree, ".card-item-set--link-title") |> get_inner_text()
-    date = Floki.find(html_tree, ".card-item-set--link-subtitle") |> get_inner_text()
+    reviewer_name = Floki.find(html_tree, ".card-item-set--link-title") |> get_inner_text()
+    link = Floki.find(html_tree, ".food-image") |> Floki.find("a") |> Floki.attribute("href") |> Enum.at(0) |> make_url()
+    date = Floki.find(html_tree, ".card-item-set--link-subtitle") |> get_inner_text() |> String.split("·") |> Enum.at(0) |> String.trim
 
     %{
       name =>
@@ -45,41 +51,16 @@ defmodule Atlas.Hound do
         "address" => address,
         "title" => title,
         "body" => body,
-        "username" => username,
-        "date" => date
+        "reviewer_name" => reviewer_name,
+        "date" => date,
+        "link" => link
       }
     }
   end
 
-  defp check_date(map) do
-    map
-    |> Map.values()
-    |> Enum.filter(fn map ->
-      String.split(map["date"])
-      |> Enum.reverse()
-      |> Enum.at(0)
-      |> check_year()
-      |> Kernel.<(2021)
-      end)
-    |> Enum.at(0)
-  end
-
-  defp check_year(year_str) do
-    try do
-      year = String.to_integer(year_str)
-    rescue
-      _ ->
-        year = 2020
-    end
-  end
-
-  defp maybe_store_and_click(map, file_path, offset) when is_map(map) do
+  defp store(map, file_path, offset) when is_map(map) do
     File.write(file_path, Jason.encode!(map) <> "\n", [:append])
   end
-
-  defp maybe_store_and_click(_, file_path, offset), do: check_visible_and_click(file_path,offset)
-
-  defp get_inner_text(html_element), do: html_element |> Floki.text() |> String.trim
 
   defp check_visible_and_click(file_path, offset) do
     button = find_element(:id, "load-more-reviews")
@@ -91,18 +72,35 @@ defmodule Atlas.Hound do
         find_all_elements(:css, ".food.card.feed-item")
         |> scrape(file_path, offset + 12)
       _ ->
+        scrape(:no_more_data)
         Hound.end_session()
     end
   end
 
-  # defp wait_page_load() do
-  #   :timer.sleep(100)
-  #   case execute_script("return document.readyState") do
-  #     "loading" ->
-  #       wait_page_load()
-  #     _ -> true
-  #   end
+  defp get_inner_text(html_element), do: html_element |> Floki.text() |> String.trim
+
+  defp make_url(link), do: "#{@base_url}#{link}"
+
+  # defp check_date(map) do
+  #   map
+  #   |> Map.values()
+  #   |> Enum.filter(fn map ->
+  #     String.split(map["date"])
+  #     |> Enum.reverse()
+  #     |> Enum.at(0)
+  #     |> check_year()
+  #     |> Kernel.<(2021)
+  #     end)
+  #   |> Enum.at(0)
   # end
 
+  # defp check_year(year_str) do
+  #   try do
+  #     year = String.to_integer(year_str)
+  #   rescue
+  #     _ ->
+  #       year = 2020
+  #   end
+  # end
 
 end
