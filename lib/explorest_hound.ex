62a41374ee78f2546cf40bca84b6dac7a014ad22
faezlatiff <muhammadal.2021@scis.smu.edu.sg>
2023-08-03 13:06:15 +0800

    scraper goes into links and stores properly

--- lib/explorest_hound.ex
+++ lib/explorest_hound.ex
@@ -8,75 +8,49 @@ defmodule Explorest.Hound do
   def run do
     Hound.start_session()
     navigate_to(@url)
-    # :timer.sleep(999999)
-
     current_window_handle()
-    |> set_window_size(1280, 637)
+    |> set_window_size(1657, 802)
 
-    scrape()
+    scrape(0)
   end
 
-  defp scrape(counter \\ 0) do
-    handle = current_window_handle()
-
-    find_all_elements(:class, "jss32")
-    # |> Enum.take(2)
-    |> Enum.each(fn elem ->
-        click(elem)
-        :timer.sleep(1000)
-        focus_window(handle)
+  defp scrape(counter) do
+    # links = []
+    elements = find_all_elements(:class, "jss35")
+    links = Enum.map(elements, fn elem ->
+      attribute_value(elem, "href")
     end)
-    # |> Enum.at(counter)
-    # |> get_info_navigate_back(counter)
 
-    :timer.sleep(100000)
+    store_info(links)
+    # scrape(counter + 1)
   end
 
-  defp get_info_navigate_back(element, counter) do
-    click(element)
-    store_info()
-    navigate_back()
-    scrape(counter+1)
+  defp check_nil(element) when not is_nil(element), do: element
+  defp check_nil(element) do
+    IO.inspect("element is nil: #{element}")
+    IO.inspect("stopping...")
+    :timer.sleep(12093123)
   end
 
+  defp inspect_navigate(link) when not is_nil(link), do: navigate_to(link)
+  defp inspect_navigate(link), do: IO.inspect("WOI LINK BROKEN: #{link}")
+
+  defp set_class(0), do: "jss35"
+  defp set_class(_), do: "jss28"
+
+  defp store_info(links) do
+    Enum.each(links, fn link ->
+      navigate_to(link)
+      store_info()
+    end)
+  end
   defp store_info() do
-    title = find_element(:tag, "h1") |> inner_text()
+    username = find_element(:class, "jss48") |> inner_text()
     map = %{
-        "title" => title
+        "username" => username
       }
 
     File.write(@file_path, Jason.encode!(map) <> "\n", [:append])
-
   end
-
-  # defp see_more() do
-
-  #   find_all_elements(:class, "article-card")
-  #   |> List.last()
-  #   |> scroll()
-
-  #   find_element(:class, "see-more")
-  #   |> check_click()
-  #   |> click()
-
-  # end
-
-  # defp find_postal(text) do
-  #   Regex.run(~r/Singapore \d{6}/, text)
-  #   |> handle_postal()
-  # end
-
-  # defp scroll(element) when not is_nil(element) do
-  #   {width, height} = element_location(element)
-  #   execute_script("window.scrollTo(#{width},#{height});")
-  #   element
-  # end
-  # defp scroll(_), do: navigate_back()
-
-  # defp handle_postal(postal) when not is_nil(postal), do: postal
-  # defp handle_postal(_), do: "no address stated"
-
-  # defp check_click(nil), do: navigate_back()
-  # defp check_click(element), do: element
 
 end
