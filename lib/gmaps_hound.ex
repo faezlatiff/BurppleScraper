84f83bb72e58b88d417b02ed5ce3007bace4ea52
faezlatiff <muhammadal.2021@scis.smu.edu.sg>
2023-08-08 15:33:35 +0800

    scraper sorts reviews

--- lib/gmaps_hound.ex
+++ lib/gmaps_hound.ex
@@ -27,13 +27,25 @@ defmodule GMaps.Hound do
     find_all_within_element(feed, :tag, "a")
     |> Enum.each(fn elem ->
       click(elem)
-      find_element(:css, "button[data-item-id='address']")
-      |> inner_text()
-      |> IO.inspect()
+      name = find_element(:tag, "h1") |> inner_text()
+      address = find_element(:css, "button[data-item-id='address']")|> inner_text()
+      reviews = get_reviews()
       wait(12312313123)
 
     end)
 
+  end
+
+  defp get_reviews() do
+    find_all_elements(:css, "button[role='tab']")
+    |> Enum.at(1)
+    |> click()
+
+    find_element(:css, "img[alt='Sort']")
+    |> click()
+
+    find_element(:css, "div[data-index='1']")
+    |> click()
   end
 
 
