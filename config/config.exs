a73bfe6818b053c75fb4b13bf28fe9a7abd34d9e
faezlatiff <muhammadal.2021@scis.smu.edu.sg>
2023-08-08 15:04:20 +0800

    added gmaps scraper, performs click; checkpoint

--- config/config.exs
+++ config/config.exs
@@ -1,10 +1,10 @@
 import Config
 
 # for burpple
-config :hound, driver: "chrome_driver", browser: "chrome_headless", server: true
+# config :hound, driver: "chrome_driver", browser: "chrome_headless", server: true
 
-# for lemon8 or explorest
-# config :hound,
-#   host: "http://localhost",
-#   port: 4444,
-#   path_prefix: "wd/hub/"
+# for lemon8, explorest and gmaps
+config :hound,
+  host: "http://localhost",
+  port: 4444,
+  path_prefix: "wd/hub/"
---
+++ lib/gmaps_hound.ex
@@ -0,0 +1,40 @@
+defmodule GMaps.Hound do
+  use Hound.Helpers
+  @url "https://www.google.com/maps"
+  @search_term "food near me"
+  @file_path "./tmp/gmaps_#{Timex.now() |> Timex.to_date() |> Timex.format("{0D}_{0M}_{YYYY}") |> elem(1)}.json"
+
+  @spec run :: list
+  def run do
+    Hound.start_session()
+    navigate_to(@url)
+    window_id = current_window_handle()
+    set_window_size(window_id, 1280, 972)
+    scrape()
+    Hound.end_session()
+  end
+
+  defp scrape() do
+    search_box = find_element(:id, "searchboxinput")
+
+    search_box
+    |> fill_field(@search_term)
+
+    find_element(:id, "cell0x0")
+    |> click()
+
+    feed = find_element(:css, "div[role='feed']")
+    find_all_within_element(feed, :tag, "a")
+    |> Enum.each(fn elem ->
+      click(elem)
+      wait(12312313123)
+
+    end)
+
+  end
+
+
+  defp wait(time \\ 1000) do
+    :timer.sleep(time)
+  end
+end
